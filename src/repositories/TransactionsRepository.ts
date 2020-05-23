70b3529becb70730889e602bbb62e5ddf14f1999
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-05-23 19:16:58 -0400

    Finished challenge

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionCreate {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,32 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .map(transaction => transaction.value)
+      .reduce((acc, cur) => acc + cur, 0);
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .map(transaction => transaction.value)
+      .reduce((acc, cur) => acc + cur, 0);
+    const total = income - outcome;
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: TransactionCreate): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
