7c87ea90c13f4084489c8682195a8cef73cd4296
Valdir Coxev [Home] <valdircoxevdev@gmail.com>
2020-05-23 20:54:16 -0400

    refactoring getBalance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -26,14 +26,21 @@ class TransactionsRepository {
 
   public getBalance(): Balance {
     // TODO
-    const income = this.transactions
-      .filter(transaction => transaction.type === 'income')
-      .map(transaction => transaction.value)
-      .reduce((acc, cur) => acc + cur, 0);
-    const outcome = this.transactions
-      .filter(transaction => transaction.type === 'outcome')
-      .map(transaction => transaction.value)
-      .reduce((acc, cur) => acc + cur, 0);
+    const { income, outcome } = this.transactions.reduce(
+      (acc, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            acc.income += transaction.value;
+            break;
+          case 'outcome':
+            acc.outcome += transaction.value;
+            break;
+          default:
+        }
+        return acc;
+      },
+      { income: 0, outcome: 0 },
+    );
     const total = income - outcome;
     return {
       income,
